5c930d56e2c82526a26ca280113220374fe8c7e7
Artem Popov <artem.popov@ourprod.local>
2025-05-09 23:06:23 +0700

    Add stages

---
+++ core/variables.tf
@@ -0,0 +1,30 @@
+variable "pm_user" {
+    type    = string
+}
+variable "pm_password" {
+    type    = string
+}
+variable "pm_endpoint" {
+    type    = string
+    description = "Proxmox API endpoint URL"
+}
+variable "pm_api_token" {
+    type    = string
+    description = "Proxmox API token"
+}
+variable "pm_template_id" {
+    type    = string
+    description = "ID of the template to clone from"
+}
+variable "pm_datastore_id" {
+    type    = string
+    description = "ID of the datastore to use"
+}
+variable "ssh_key_path" {
+  type    = string  
+}
+variable "root_password" {
+  type        = string
+  sensitive   = true
+  default     = "root123"  # Рекомендуется изменить на более безопасный пароль
+}
