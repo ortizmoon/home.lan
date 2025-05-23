f0d769af3fdc38646d4af8ee9862483ae79f5004
Artem Popov <artem.popov@ourprod.local>
2025-05-23 14:48:13 +0700

    Add ip-vars in terraform

---
+++ reinc.sh
@@ -0,0 +1,4 @@
+cd /Users/doctormetis/git/my_git/home.lan/terraform/bootstrap
+terraform init
+terraform destroy -auto-approve
+terraform apply -auto-approve
--- terraform/bootstrap/inventory.tf
+++ terraform/bootstrap/inventory.tf
@@ -1,5 +1,6 @@
 resource "local_file" "ansible_inventory" {
   content  = templatefile("${path.module}/inventory.yml.tmpl", {
+<<<<<<< HEAD
     # consul_ip  = split("/", proxmox_virtual_environment_vm.consul.ipv4_addresses[1][0])[0]
     # traefik_ip = split("/", proxmox_virtual_environment_vm.traefik.ipv4_addresses[1][0])[0]
     # netbox_ip  = split("/", proxmox_virtual_environment_vm.netbox.ipv4_addresses[1][0])[0]
@@ -8,6 +9,12 @@ resource "local_file" "ansible_inventory" {
     netbox_ip       = var.netbox_ip
 
     ssh_privatekey  = var.ssh_privatekey
+=======
+    consul_ip  = var.consul_ip
+    traefik_ip = var.traefik_ip
+    netbox_ip  = var.netbox_ip
+    ssh_privatekey = var.ssh_privatekey
+>>>>>>> 6d361f6 (Add ip-vars in terraform)
 
     consul_domain   = var.consul_domain
     traefik_domain  = var.traefik_domain
